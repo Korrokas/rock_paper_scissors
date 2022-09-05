58be43ffa01be87468380792ba240f3c59560bfb
Brandon <kounse@gmail.com>
2022-09-04 22:54:50 -0400

    Reworked rock, paper, scissors into a functional GUI with event listeners

--- code.js
+++ code.js
@@ -1,99 +1,99 @@
+// defined the variables to grab the button id's and assign value
+const rock = document.querySelector("#rock");
+const paper = document.querySelector("#paper");
+const scissors = document.querySelector("#scissors");
+
+const results = document.querySelector("#results");
+const score = document.querySelector('#score');
+
+let playerTotal = 0;
+let computerTotal = 0;
+
+// tying to buttons with eventListeners below
+
+rock.addEventListener('click', playRound);
+paper.addEventListener('click', playRound);
+scissors.addEventListener('click', playRound);
+
+//computer choice selection
+
 function getComputerChoice() {
     let guess = Math.floor(Math.random() * 3) + 1;
     return guess;
 }
 
-function getPlayerChoice() {
-    let playerChoice = prompt("Please choose rock, paper, or scissors: ");
-    playerChoice = playerChoice.toLowerCase();
+//the game logic to determine the winner each round
 
-    if (playerChoice == "rock" || playerChoice == "paper" || playerChoice == "scissors") {
-        return playerChoice;
-    }
-    else {
-        console.log("Invalid choice, please choose rock, paper, or scissors: ");
-        return getPlayerChoice();
-    }
-}
+function playRound() {
+    const playerSelection = this.textContent.toLowerCase();
+    const computerSelection = getComputerChoice();
 
-function playRound(playerSelection, computerSelection) {
     let roundWinner = "";
-
    
-    if (computerSelection == 1) {
-        if (playerSelection == "rock") {
-            console.log(`Computer chose rock, you chose ${playerSelection}.`);
+    if (computerSelection === 1) {
+        if (playerSelection === "rock") {
+            results.textContent = `Computer chose rock, you chose ${playerSelection}. It's a tie!`;
         }
-        else if (playerSelection == "paper") {
+        else if (playerSelection === "paper") {
             roundWinner = "Player";
-            console.log(`Computer chose rock, you chose ${playerSelection}. ${roundWinner} wins!`);
+            results.textContent = `Computer chose rock, you chose ${playerSelection}. ${roundWinner} wins!`;
         }
         else {
             roundWinner = "Computer";
-            console.log(`Computer chose rock, you chose ${playerSelection}. ${roundWinner} wins!`);
+            results.textContent = `Computer chose rock, you chose ${playerSelection}. ${roundWinner} wins!`;
         }
     }
 
-    if (computerSelection == 2) {
-        if (playerSelection == "rock") {
+    if (computerSelection === 2) {
+        if (playerSelection === "rock") {
             roundWinner = "Computer";
-            console.log(`Computer chose paper, you chose ${playerSelection}. ${roundWinner} wins!`);
+            results.textContent = `Computer chose paper, you chose ${playerSelection}. ${roundWinner} wins!`;
         }
-        else if (playerSelection == "paper") {
-            console.log(`Computer chose paper, you chose ${playerSelection}.`);
+        else if (playerSelection === "paper") {
+            results.textContent = `Computer chose paper, you chose ${playerSelection}. It's a tie!`;
         }
         else {
             roundWinner = "Player";
-            console.log(`Computer chose paper, you chose ${playerSelection}. ${roundWinner} wins!`);
+            results.textContent = `Computer chose paper, you chose ${playerSelection}. ${roundWinner} wins!`;
         }
     }
 
-    if (computerSelection == 3) {
-        if (playerSelection == "rock") {
+    if (computerSelection === 3) {
+        if (playerSelection === "rock") {
             roundWinner = "Player";
-            console.log(`Computer chose scissors, you chose ${playerSelection}. ${roundWinner} wins!`);
+            results.textContent = `Computer chose scissors, you chose ${playerSelection}. ${roundWinner} wins!`;
         }
-        else if (playerSelection == "paper") {
+        else if (playerSelection === "paper") {
             roundWinner = "Computer";
-            console.log(`Computer chose scissors, you chose ${playerSelection}. ${roundWinner} wins!`);
+            results.textContent = `Computer chose scissors, you chose ${playerSelection}. ${roundWinner} wins!`;
         }
         else {
-            console.log(`Computer chose scissors, you chose ${playerSelection}.`);
+            results.textContent = `Computer chose scissors, you chose ${playerSelection}. It's a tie!`;
         }
+    }
+
+    if (roundWinner === "Player") {
+        playerTotal++;
+        document.querySelector("#playerScore").textContent = `Player: ${playerTotal}`;
+    }
+    else if (roundWinner === "Computer") {
+        computerTotal++;
+        document.querySelector("#computerScore").textContent = `Computer: ${computerTotal}`;
     }
 
     return roundWinner;
 }
 
-
-function game() {
-    let pScore = 0;
-    let cScore = 0;
-
-    for (i = 0; i < 5; i++) {
-        let result = playRound(getPlayerChoice(), getComputerChoice());
-
-        if (result == "Player") {
-            pScore++;
-        }
-        else if (result == "Computer") {
-            cScore++;
+const determineWinner = document.addEventListener('click', e => {
+    if (computerTotal === 5 || playerTotal === 5) {
+        if (computerTotal === 5) {
+            results.textContent = "COMPUTER WINS!";
         }
         else {
-            console.log("This round is a tie!");
+            results.textContent = "YOU WIN!";
         }
-        console.log(`Player Score: ${pScore} | Computer Score: ${cScore}`);
+        rock.removeEventListener('click', playRound);
+        paper.removeEventListener('click', playRound);
+        scissors.removeEventListener('click', playRound);
     }
-    
-    if (pScore > cScore) {
-        console.log("Player has won best of 5!");
-    }
-    else if (cScore > pScore) {
-        console.log("Computer has won best of 5!");
-    }
-    else {
-        console.log("The best of 5 is a tie!");
-    }
-}
-
-game();
+});
